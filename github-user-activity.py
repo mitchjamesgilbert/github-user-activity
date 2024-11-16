54bf4cde8653ac87138995f7f1bcb3c017429ad8
mitch <mitch.james.gilbert@gmail.com>
2024-11-16 21:36:59 +0000

    Add github API request logic

--- github-user-activity.py
+++ github-user-activity.py
@@ -1,4 +1,6 @@
 import sys
+import urllib.request
+import json
 
 if len(sys.argv) != 2:
     print("Usage: python github-user-activity.py <username>")
@@ -7,3 +9,17 @@ if len(sys.argv) != 2:
 username = sys.argv[1]
 print(f"Fetching activity for {username}")
 
+url = f"https://api.github.com/users/{username}/events"
+
+try:
+    with urllib.request.urlopen(url) as response:
+        data = json.load(response)
+    
+        print(json.dumps(data, indent=4))
+
+except urllib.error.HTTPError as e:
+    print(f"HTTP Error: {e.code} - {e.reason}")
+except urllib.error.URLError as e:
+    print(f"URL Error: {e.reason}")
+except Exception as e:
+    print(f"Unexpected Error: {e}")
